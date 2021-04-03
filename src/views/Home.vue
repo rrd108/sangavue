8048bb7f9ae9e680e9206946ac000284c3f73fb3
rrd <rrd@1108.cc>
2021-04-03 10:48:00 +0200

    use token

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,26 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
   name: 'Home',
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    axios.get(`${process.env.VUE_APP_API_URL}users/index.json`, {
+      headers: {
+        'Authorization': `Token ${this.$store.state.token}`
+      }
+    })
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
 }
 </script>
